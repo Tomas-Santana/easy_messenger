0aac1c9cdb223690fe7e5988ce2b550589322249
Tomas Santana <tomas.30604530@uru.edu>
2023-08-05 12:42:44 -0400

    Finalized setup

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-
 from distutils.core import setup
 setup(
-  name = 'easy_messenger',         # How you named your package folder (MyLib)
-  packages = ['easy_messenger'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'The simplest Messenger (Facebook, Instagram) API wrapper out there',   # Give a short description about your library
-  author = 'Tomas Santana',                   # Type in your name
-  author_email = 'tommysantana5@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Tomas-Santana/easy_messenger',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Instagram', 'Facebook', 'API', 'Wrapper'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'easy_messenger',        
+  packages = ['easy_messenger'],   
+  version = '0.1',     
+  license='MIT',        
+  description = 'The simplest Messenger (Facebook, Instagram) API wrapper out there',   
+  author = 'Tomas Santana',               
+  author_email = 'tommysantana5@gmail.com',    
+  url = 'https://github.com/Tomas-Santana/',   
+  download_url = 'https://github.com/Tomas-Santana/easy_messenger/archive/refs/tags/v0.1.tar.gz',    
+  keywords = ['Instagram', 'Facebook', 'API', 'Wrapper'],  
+  install_requires=[            
           'requests',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
   ],)
